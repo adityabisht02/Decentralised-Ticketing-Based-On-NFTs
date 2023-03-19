e8d30f2ca17a047ca3513891609758f593752ff3
Aditya <adityabisht02@gmail.com>
2023-03-20 00:18:05 +0530

    font change

--- client/src/Organiser/Home/Instructions.js
+++ client/src/Organiser/Home/Instructions.js
@@ -3,11 +3,11 @@ import React from "react";
 function Instructions() {
   return (
     <div className="relative flex flex-col justify-center overflow-hidden bg-gray-50 py-3 px-3 sm:py-12 ">
-      <div className="relative bg-white px-3 pt-3 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+      <div className="relative bg-white px-3 pt-3 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto  sm:rounded-lg sm:px-10">
         <div className="mx-auto ">
           <div className="divide-y divide-gray-300/50">
             <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
-              <p className="text-center font-bold ">WHY NFT TICKETS?</p>
+              <p className="text-center font-bold text-2xl">WHY NFT TICKETS?</p>
               <ul className="space-y-4">
                 <li className="flex items-center">
                   <svg
@@ -21,7 +21,7 @@ function Instructions() {
                       fill="none"
                     />
                   </svg>
-                  <p className="ml-4">
+                  <p className="ml-4 text-xl">
                     Prevents fake ticket frauds since tokens exist on chain.
                   </p>
                 </li>
@@ -37,7 +37,7 @@ function Instructions() {
                       fill="none"
                     />
                   </svg>
-                  <p className="ml-4">
+                  <p className="ml-4 text-xl">
                     Act as great collectibles and have value.
                   </p>
                 </li>
@@ -53,7 +53,7 @@ function Instructions() {
                       fill="none"
                     />
                   </svg>
-                  <p className="ml-4">
+                  <p className="ml-4 text-xl">
                     Prevents black marketing since tickets cannot be sold for
                     higher price
                   </p>
@@ -70,7 +70,7 @@ function Instructions() {
                       fill="none"
                     />
                   </svg>
-                  <p className="ml-4">
+                  <p className="ml-4 text-xl">
                     Has future benefits in case organisers wish to award NFT
                     holders.
                   </p>
@@ -87,7 +87,7 @@ function Instructions() {
                       fill="none"
                     />
                   </svg>
-                  <p className="ml-4">
+                  <p className="ml-4 text-xl">
                     Helps celebrities get closer to their audience.
                   </p>
                 </li>
